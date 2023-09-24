f1a6822ff7075025c6610244ecf1b6fb3b9cbcce
Theo Roque <theoroque95@gmail.com>
2023-09-25 00:21:49 +0800

    move utils to a its dedicated folder

--- lib/utils/isPreviousDate.ts
+++ lib/utils/isPreviousDate.ts
@@ -1,10 +1,3 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
 /**
  * Determines if a given date string is from a previous date.
  *
