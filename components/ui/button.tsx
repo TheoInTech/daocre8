3961b22428a423c130ec66da02ad6776b8cb85fa
Theo Roque <theoroque95@gmail.com>
2023-09-24 10:01:46 +0800

    added xs to button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -24,6 +24,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "px-8 py-4",
+        xs: "h-7 rounded-md px-2 text-xs",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-lg px-8",
         icon: "h-10 w-10",
