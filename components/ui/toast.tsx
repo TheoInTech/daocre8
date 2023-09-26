383d643ff56c43f43107dfdd36844d00555ba4c2
Theo Roque <theoroque95@gmail.com>
2023-09-26 12:57:55 +0800

    added success to toast

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -28,6 +28,7 @@ const toastVariants = cva(
     variants: {
       variant: {
         default: "border bg-background text-foreground",
+        success: "border bg-success text-muted",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
