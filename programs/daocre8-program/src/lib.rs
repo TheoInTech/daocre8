6e34f615b12cc3132c709e10ec9f35052a2f87ff
Theo Roque <theoroque95@gmail.com>
2023-09-27 00:40:06 +0800

    continue fixing anchor build issue

--- programs/daocre8-program/src/lib.rs
+++ programs/daocre8-program/src/lib.rs
@@ -10,13 +10,27 @@ pub mod escrow;
 pub mod polls;
 
 use errors::*;
-use project_dao::InitializeProjectDao;
-use escrow::InitializeEscrow;
+use project_dao::{
+    state::ProjectDaoAccount,
+    InitializeProjectDao,
+    initialize_project_dao,
+};
+use escrow::{
+    state::EscrowAccount,
+    InitializeEscrow,
+    initialize_escrow,
+};
 use polls::state::{
+    DecisionMakingPoll,
     InitializeDecisionMakingPoll,
+    initialize_decision_making_poll,
+    MilestoneAchievementPoll,
     InitializeMilestoneAchievementPoll,
+    initialize_milestone_achievement_poll,
     VoteInDecisionMakingPoll,
+    vote_in_decision_making_poll,
     VoteInMilestoneAchievementPoll,
+    vote_in_milestone_achievement_poll,
 };
 
 declare_id!("ATqP7TbxPzSYd2nTbXo1usji5Ftq4QRae93SvDzD89yy");
@@ -30,54 +44,54 @@ pub mod daocre8 {
         ctx: Context<InitializeProjectDao>,
         project_ipfs_hash: String,
     ) -> Result<()> {
-        project_dao::initialize_project_dao(ctx, project_ipfs_hash)
+        initialize_project_dao(ctx, project_ipfs_hash)
     }
 
     pub fn initialize_escrow(ctx: Context<InitializeEscrow>, amount: u64) -> Result<()> {
-        escrow::initialize_escrow(ctx, amount)
+        initialize_escrow(ctx, amount)
     }
 
-    pub fn initialize_decision_making_poll(
-        ctx: Context<InitializeDecisionMakingPoll>,
-        question: String,
-        options: Vec<String>,
-        start_datetime: i64,
-        end_datetime: i64,
-    ) -> Result<()> {
-        polls::initialize_decision_making_poll(
-            ctx,
-            question,
-            options,
-            start_datetime,
-            end_datetime,
-        )
-    }
+    // pub fn initialize_decision_making_poll(
+    //     ctx: Context<InitializeDecisionMakingPoll>,
+    //     question: String,
+    //     options: Vec<String>,
+    //     start_datetime: i64,
+    //     end_datetime: i64,
+    // ) -> Result<()> {
+    //     polls::initialize_decision_making_poll(
+    //         ctx,
+    //         question,
+    //         options,
+    //         start_datetime,
+    //         end_datetime,
+    //     )
+    // }
 
-    pub fn initialize_milestone_achievement_poll(
-        ctx: Context<InitializeMilestoneAchievementPoll>,
-        milestone_id: u8,
-        start_datetime: i64,
-        end_datetime: i64,
-    ) -> Result<()> {
-        polls::initialize_milestone_achievement_poll(
-            ctx,
-            milestone_id,
-            start_datetime,
-            end_datetime,
-        )
-    }
+    // pub fn initialize_milestone_achievement_poll(
+    //     ctx: Context<InitializeMilestoneAchievementPoll>,
+    //     milestone_id: u8,
+    //     start_datetime: i64,
+    //     end_datetime: i64,
+    // ) -> Result<()> {
+    //     polls::initialize_milestone_achievement_poll(
+    //         ctx,
+    //         milestone_id,
+    //         start_datetime,
+    //         end_datetime,
+    //     )
+    // }
 
-    pub fn vote_in_decision_making_poll(
-        ctx: Context<VoteInDecisionMakingPoll>,
-        option_index: u8,
-    ) -> Result<()> {
-        polls::vote_in_decision_making_poll(ctx, option_index)
-    }
+    // pub fn vote_in_decision_making_poll(
+    //     ctx: Context<VoteInDecisionMakingPoll>,
+    //     option_index: u8,
+    // ) -> Result<()> {
+    //     polls::vote_in_decision_making_poll(ctx, option_index)
+    // }
 
-    pub fn vote_in_milestone_achievement_poll(
-        ctx: Context<VoteInMilestoneAchievementPoll>,
-        vote: bool,
-    ) -> Result<()> {
-        polls::vote_in_milestone_achievement_poll(ctx, vote)
-    }
+    // pub fn vote_in_milestone_achievement_poll(
+    //     ctx: Context<VoteInMilestoneAchievementPoll>,
+    //     vote: bool,
+    // ) -> Result<()> {
+    //     polls::vote_in_milestone_achievement_poll(ctx, vote)
+    // }
 }
