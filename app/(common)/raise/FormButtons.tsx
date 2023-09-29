298680f2cb968b49514ee997fbbdc3e9bd80071d
Theo Roque <theoroque95@gmail.com>
2023-09-30 00:47:42 +0800

    uncomment complete button

--- app/(common)/raise/FormButtons.tsx
+++ app/(common)/raise/FormButtons.tsx
@@ -77,18 +77,18 @@ export const FormButtons = ({
           <Button onClick={handleBack} variant={"outline"} size={"md"}>
             <ChevronLeft className="w-5 h-5 mr-4" /> Back
           </Button>
-          {/* {Object.values(completion).filter(Boolean).length ===
-            Object.keys(completion).length && ( */}
-          <Button
-            onClick={onSubmit}
-            type="submit"
-            disabled={disabled}
-            size={"md"}
-          >
-            Complete
-            <ChevronRight className="w-5 h-5 ml-4" />
-          </Button>
-          {/* )} */}
+          {Object.values(completion).filter(Boolean).length ===
+            Object.keys(completion).length && (
+            <Button
+              onClick={onSubmit}
+              type="submit"
+              disabled={disabled}
+              size={"md"}
+            >
+              Complete
+              <ChevronRight className="w-5 h-5 ml-4" />
+            </Button>
+          )}
         </>
       );
   }
