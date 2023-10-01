784f939392eb18646c5ccde96ae7b34ae864d9cd
Theo Roque <theoroque95@gmail.com>
2023-10-02 07:59:11 +0800

    fix hover for outlined button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-border bg-background text-muted-foreground hover:bg-accent hover:text-accent-foreground",
+          "border border-border bg-background text-muted-foreground hover:bg-muted/10",
         secondary:
           "bg-secondary text-secondary-foreground text-muted hover:bg-secondary/80",
         ghost: "text-muted hover:text-muted/70",
