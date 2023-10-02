5730a46c676b3eca003c0a6f92389bcdf76b7467
Theo Roque <theoroque95@gmail.com>
2023-10-02 23:22:23 +0800

    start poll

--- app/creator/your-projects/[projectAddress]/Milestones.tsx
+++ app/creator/your-projects/[projectAddress]/Milestones.tsx
@@ -19,21 +19,36 @@ export const Milestones = () => {
       {milestones?.map((milestone) => (
         <div
           key={milestone.description}
-          className="flex flex-col gap-4 border-b border-b-muted/20 pb-8 last:border-b-0"
+          className="grid grid-cols-1 md:grid-cols-3 border-b border-b-muted/20 pb-8 last:border-b-0"
         >
-          <div className="flex flex-col gap-2 ">
-            <div className="text-sm font-medium text-primary">Description</div>
-            <div className="text-base font-medium">{milestone.description}</div>
-          </div>
-          <div className="flex flex-col gap-2">
-            <div className="text-sm font-medium text-primary">Amount</div>
-            <div className="text-base font-medium">
-              {(fundingAmount * (milestone.percentage / 100)).toLocaleString()}{" "}
-              {project?.project_ipfs_hash?.fundingAndMilestones.currency}
+          <div className="flex flex-col gap-4 col-span-1 md:col-span-2">
+            <div className="flex flex-col gap-2 ">
+              <div className="text-sm font-medium text-primary">
+                Milestone description
+              </div>
+              <div className="text-base font-medium">
+                {milestone.description} Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Error unde necessitatibus rerum praesentium
+                explicabo quis vitae temporibus odit accusantium ratione
+                voluptate atque eaque doloremque recusandae, porro at fugit,
+                dicta repellat.
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <div className="text-sm font-medium text-primary">
+                Amount to unlock
+              </div>
+              <div className="text-base font-medium">
+                {(
+                  fundingAmount *
+                  (milestone.percentage / 100)
+                ).toLocaleString()}{" "}
+                {project?.project_ipfs_hash?.fundingAndMilestones.currency}
+              </div>
             </div>
           </div>
-          <Button className="w-fit" size={"md"}>
-            Start Milestone Poll
+          <Button className="w-fit h-fit justify-self-end" size={"sm"}>
+            Start milestone poll
           </Button>
         </div>
       ))}
