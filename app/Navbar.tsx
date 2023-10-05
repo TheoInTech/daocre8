6b6d0cb709aed423759297017cda05989ceb0cc6
Theo Roque <theoroque95@gmail.com>
2023-10-05 23:34:11 +0800

    Change hover for backer

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -6,15 +6,6 @@ import { usePathname } from "next/navigation";
 import { Link } from "nextjs13-progress";
 // components
 import { ConnectWallet } from "@/app/ConnectWallet";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils/cn";
 import LogoIcon from "@/public/logo.svg";
 import { useWallet } from "@solana/wallet-adapter-react";
@@ -25,14 +16,14 @@ export const navItems: { title: string; href: string }[] = [
     title: "Home",
     href: "/",
   },
-  {
-    title: "Fund a Project",
-    href: "/fund",
-  },
-  {
-    title: "Fundraise",
-    href: "/raise",
-  },
+  // {
+  //   title: "Fund a Project",
+  //   href: "/fund",
+  // },
+  // {
+  //   title: "Fundraise",
+  //   href: "/raise",
+  // },
   {
     title: "Join Waitlist",
     href: "https://docs.google.com/forms/d/e/1FAIpQLSeIUD7FSluhW2QjspFW7R9YU1VVMBtfPBPvJX2YD5LqdwYiXA/viewform",
@@ -50,7 +41,7 @@ export const Navbar = () => {
 
   return (
     <div className="fixed left-0 top-0 right-0 z-50">
-      <div className="flex flex-wrap justify-center text-xs items-center p-2 bg-accent text-foreground font-medium">
+      {/* <div className="flex flex-wrap justify-center text-xs items-center p-2 bg-accent text-foreground font-medium">
         This is a demo site for Solana Hyperdrive and mock data are currently
         used. Be the first to know when we launch!{" "}
         <Link
@@ -60,7 +51,7 @@ export const Navbar = () => {
         >
           Join our waitlist.
         </Link>
-      </div>
+      </div> */}
       <div className="flex px-4 md:px-20 text-lg py-4 bg-black/80 items-center justify-between backdrop-blur-sm shadow-md">
         <Link
           href="/"
@@ -96,36 +87,6 @@ export const Navbar = () => {
               </div>
             ))}
             <ConnectWallet />
-            {publicKey && (
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <Avatar className="h-14 w-14 hover:brightness-105 hover:scale-110 duration-300 ease-in-out">
-                    {/* TODO: Change to actual avatar or NFT */}
-                    <AvatarImage src="/" alt="8" />
-                    {/* TODO: Change to name initials */}
-                    <AvatarFallback className="bg-primary/20 text-sm">
-                      You
-                    </AvatarFallback>
-                  </Avatar>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem asChild onClick={handleMenuClick}>
-                    <Link href={"/creator"}>Creator Profile</Link>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem asChild onClick={handleMenuClick}>
-                    <Link href={"/creator/your-projects"}>Your Projects</Link>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem asChild onClick={handleMenuClick}>
-                    <Link href={"/backed-projects"}>Backed Projects</Link>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem asChild onClick={handleMenuClick}>
-                    <Link href={"/settings"}>Settings</Link>
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            )}
           </div>
         </div>
       </div>
