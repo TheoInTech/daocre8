6c02f657979e6ff32778b211934546ef2f64262a
Theo Roque <theoroque95@gmail.com>
2023-10-05 23:58:13 +0800

    Adjust fund

--- app/(common)/fund/[projectAddress]/ProjectDetails.tsx
+++ app/(common)/fund/[projectAddress]/ProjectDetails.tsx
@@ -54,17 +54,17 @@ export const ProjectDetails = () => {
         <div className="flex gap-4 items-start w-full">
           {xUrl && (
             <Link href={xUrl} target="_blank">
-              <Twitter className="w-6 h-6 hover:text-secondary duration-300 stroke-2" />
+              <Twitter className="w-6 h-6 hover:text-secondary duration-300 stroke-1" />
             </Link>
           )}
           {linkedinUrl && (
             <Link href={linkedinUrl} target="_blank">
-              <Linkedin className="w-6 h-6 hover:text-secondary duration-300 stroke-2" />
+              <Linkedin className="w-6 h-6 hover:text-secondary duration-300 stroke-1" />
             </Link>
           )}
           {githubUrl && (
             <Link href={githubUrl} target="_blank">
-              <Github className="w-6 h-6 hover:text-secondary duration-300 stroke-2" />
+              <Github className="w-6 h-6 hover:text-secondary duration-300 stroke-1" />
             </Link>
           )}
         </div>
