85ec9229c9365c319aa1c65b148d94445e0f862c
Theo Roque <theoroque95@gmail.com>
2023-10-07 13:37:42 +0800

    Add no polls yet

--- app/(backer)/backed-projects/[projectAddress]/Polls.tsx
+++ app/(backer)/backed-projects/[projectAddress]/Polls.tsx
@@ -36,66 +36,70 @@ export const Polls = () => {
         options={activePoll?.options ?? []}
       />
       <div className="flex flex-col gap-8">
-        {changePolls?.map((poll) => {
-          const hasPollStarted = new Date(poll.start_datetime) < new Date();
+        {changePolls.length > 0 ? (
+          changePolls?.map((poll) => {
+            const hasPollStarted = new Date(poll.start_datetime) < new Date();
 
-          return (
-            <div
-              key={poll.idx}
-              className="card-glass grid grid-cols-1 md:grid-cols-2 gap-8 items-center"
-            >
-              <Detail
-                title={"Poll starts in"}
-                value={
-                  <CountdownTimer
-                    className="font-bold"
-                    endTime={poll.start_datetime}
-                    endText={format(
-                      new Date(poll.start_datetime),
-                      "dd MMM yyyy HH:mm"
-                    )}
-                  />
-                }
-              />
-              <Detail
-                title={"Poll ending in"}
-                value={
-                  <CountdownTimer
-                    className="font-bold"
-                    endTime={poll.end_datetime}
-                  />
-                }
-              />
-              <Detail
-                title={"Poll question"}
-                value={poll.question}
-                className="col-span-1 md:col-span-2"
-              />
-              <Detail
-                title={"Answer options"}
-                value={
-                  <ul>
-                    {poll.options.map((option) => (
-                      <li key={option} className="list-disc list-inside">
-                        {option}
-                      </li>
-                    ))}
-                  </ul>
-                }
-                className="col-span-1 md:col-span-2"
-              />
-              <Button
-                variant="secondary"
-                size={"md"}
-                className="w-fit"
-                disabled={!hasPollStarted}
-                onClick={() => handleClickPoll(poll)}
+            return (
+              <div
+                key={poll.idx}
+                className="card-glass grid grid-cols-1 md:grid-cols-2 gap-8 items-center"
               >
-                Submit your vote
-              </Button>
-            </div>
-          );
-        })}
+                <Detail
+                  title={"Poll starts in"}
+                  value={
+                    <CountdownTimer
+                      className="font-bold"
+                      endTime={poll.start_datetime}
+                      endText={format(
+                        new Date(poll.start_datetime),
+                        "dd MMM yyyy HH:mm"
+                      )}
+                    />
+                  }
+                />
+                <Detail
+                  title={"Poll ending in"}
+                  value={
+                    <CountdownTimer
+                      className="font-bold"
+                      endTime={poll.end_datetime}
+                    />
+                  }
+                />
+                <Detail
+                  title={"Poll question"}
+                  value={poll.question}
+                  className="col-span-1 md:col-span-2"
+                />
+                <Detail
+                  title={"Answer options"}
+                  value={
+                    <ul>
+                      {poll.options.map((option) => (
+                        <li key={option} className="list-disc list-inside">
+                          {option}
+                        </li>
+                      ))}
+                    </ul>
+                  }
+                  className="col-span-1 md:col-span-2"
+                />
+                <Button
+                  variant="secondary"
+                  size={"md"}
+                  className="w-fit"
+                  disabled={!hasPollStarted}
+                  onClick={() => handleClickPoll(poll)}
+                >
+                  Submit your vote
+                </Button>
+              </div>
+            );
+          })
+        ) : (
+          <span>No polls yet.</span>
+        )}
       </div>
     </>
   );
